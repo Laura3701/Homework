1353039cdf5e3b036e55a762a598367c4351ea2b
Shelby <shelby@ShelbydeMacBook-Air.local>
2024-10-26 23:48:54 +0800

    fix: 修改

--- 第六周/Day1/js/my.js
+++ 第六周/Day1/js/my.js
@@ -1,2 +1,14 @@
-alert('我是外部的js文件')
-alert('work hard')
+let sum1 = 0
+let sum2 = 0
+for (i = 1; i <= 10; i++) {
+  switch (i) {
+    case (i / 2 == 0):
+      sum1 += i
+      break
+    default:
+      sum2 += i
+      break
+  }
+}
+
+console.log(sum1, sum2)
